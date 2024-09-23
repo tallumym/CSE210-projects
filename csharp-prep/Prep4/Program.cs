63d90b836f31cbb55805019b4e60e5af71b3b87f
tallumym <mil23049@byui.edu>
2024-09-23 10:57:03 -0400

    Next push

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,47 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    //part1 - Compute
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber !=0)
+        {
+            Console.Write("Enter a number, then I will give you the sum of all - 0 to quit: ");
+             
+             string userResponse = Console.ReadLine();
+             userNumber = int.Parse(userResponse);
+
+             if (userNumber != 0)
+             {
+                numbers.Add(userNumber);
+             }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        //part2 - Average
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+        //part3 - Max
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        int userNumber = -1;
+        Console.WriteLine("Welcome to the program!");
+
+        Console.WriteLine("What is your name? ");
+        string first = Console.ReadLine();
+        Console.WriteLine($"Your name is {first}");
+
+        Console.Write("What is your favorite number? ");
+        userNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Your favorite number is {userNumber}");
+
+        int result = userNumber * userNumber;
+        Console.WriteLine($"The square of your favorite number is {result}");
+
+        Console.WriteLine($"Your name is {first}, your favorite number is {userNumber}, and your favorite number squared is {result} ");
     }
 }
